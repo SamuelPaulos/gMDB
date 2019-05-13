1dd0e3c2c7d373e1171de4d378b9a8ab49540f71
Training <vidya.sagar554@gmail.com>
2019-05-13 16:54:37 -0400

    controller and domain created

--- user-gmdb-service/src/main/java/com/cognizant/usergmdbservice/UserGmdbServiceApplication.java
+++ user-gmdb-service/src/main/java/com/cognizant/usergmdbservice/UserGmdbServiceApplication.java
@@ -10,4 +10,6 @@ public class UserGmdbServiceApplication {
 		SpringApplication.run(UserGmdbServiceApplication.class, args);
 	}
 
+
+
 }
--- user-gmdb-service/src/main/java/com/cognizant/usergmdbservice/controller/UserController.java
+++ user-gmdb-service/src/main/java/com/cognizant/usergmdbservice/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.cognizant.usergmdbservice.controller;
 
+import com.cognizant.usergmdbservice.domain.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class UserController {
+
+    @GetMapping(value = "/")
+    public String getHome(){
+        return "congra";
+    }
+
+    @PostMapping(value = "/createuser")
+    public String createUser(@RequestBody User user){
+        System.out.println(user);
+        return "thank you!";
+    }
 }
