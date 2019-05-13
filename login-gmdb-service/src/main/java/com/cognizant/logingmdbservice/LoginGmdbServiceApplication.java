aae9e1c5c92f3b25a9dd40c23803efa29624e4d8
Training <vidya.sagar554@gmail.com>
2019-05-13 11:55:01 -0400

    inital update

---
+++ login-gmdb-service/src/main/java/com/cognizant/logingmdbservice/LoginGmdbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.logingmdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginGmdbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginGmdbServiceApplication.class, args);
+	}
+
+}
